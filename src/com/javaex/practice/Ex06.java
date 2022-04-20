0ab65550279a7725399d077a58d5beeb25d54977
ujiun <ujiun@hanmail.net>
2022-04-20 21:27:03 +0900

    6번문제

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -4,7 +4,15 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
 		
+		System.out.println(c);
+		
+		for(int i=0; i<c.length; i++) {
+			if(c[i]==' ') {
+				c[i] =',';						
+			}			
+		}
+		System.out.println(c);
 	}
-
 }
