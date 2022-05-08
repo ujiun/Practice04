0677796833929df65a77a1bdf8c40b3640dcef24
ujiun <ujiun@hanmail.net>
2022-05-08 11:53:33 +0900

    2

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -1,10 +1,25 @@
+
 package com.javaex.practice;
 
 public class Ex08 {
 
 	public static void main(String[] args) {
 		
-
+		int[] no = new int[6];
+		
+		for(int i = 0; i<no.length; i++) {
+			no[i] = (int)(Math.random()*45)+1;
+			
+			for(int j=0; j<i; j++) {
+				if(no[i] == no[j]) {
+						--i;
+						break;
+				}
+			}
+		}
+		for(int j =0; j< no.length; j++) {
+			System.out.print(no[j] + " ");
+		}
+		
 	}
-
 }
